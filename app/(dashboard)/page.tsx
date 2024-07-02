dfdbc66421c2e268a0f73cabd093a83f4557b8bf
= <emailaddr>
2024-07-02 13:44:08 +0500

    authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="m-5">
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
